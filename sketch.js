0bfde66a65afa4da197d2523eaa457cbe308851d
ArkMan13 <74531609+ArkMan13@users.noreply.github.com>
2021-02-24 17:06:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvas,backgroundImg;
+var allPlayers;
+var gameState = 0;
+var playerCount;
+
+var database;
+
+var name, player, form, game;
+
+function setup(){
+    createCanvas(400,400);
+    database=firebase.database();
+
+    game = new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    
+}
+
